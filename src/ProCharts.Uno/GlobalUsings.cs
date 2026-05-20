1764fef56808283a611185f5a2ffc00c935d8b38
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-05-20 19:05:37 +0200

    refactor: Add global using aliases and SkiaSharp mock namespace

--- src/ProCharts.Uno/GlobalUsings.cs
+++ src/ProCharts.Uno/GlobalUsings.cs
@@ -13,3 +13,17 @@ global using CombinedGeometry = ProCharts.Uno.Maths.CombinedGeometry;
 global using GeometryCombineMode = ProCharts.Uno.Maths.GeometryCombineMode;
 global using SweepDirection = ProCharts.Uno.Maths.SweepDirection;
 global using Matrix = ProCharts.Uno.Maths.Matrix;
+
+global using Brush = Microsoft.UI.Xaml.Media.Brush;
+global using SolidColorBrush = Microsoft.UI.Xaml.Media.SolidColorBrush;
+global using LinearGradientBrush = Microsoft.UI.Xaml.Media.LinearGradientBrush;
+global using GradientStop = ProCharts.Uno.GradientStop;
+global using Color = Windows.UI.Color;
+global using Pen = ProCharts.Uno.Pen;
+global using StreamGeometry = ProCharts.Uno.Maths.StreamGeometry;
+global using StreamGeometryContext = ProCharts.Uno.Maths.PathOpenContext;
+global using DrawingContext = ProCharts.Uno.Maths.DrawingContext;
+
+global using SKCanvas = ProCharts.Uno.Maths.DrawingContext;
+global using SKPath = ProCharts.Uno.Maths.StreamGeometry;
+global using SKRect = ProCharts.Uno.Maths.Rect;
---
+++ src/ProCharts.Uno/Styles/SkiaSharpDummy.cs
@@ -0,0 +1,7 @@
+namespace SkiaSharp
+{
+    // Dummy class to satisfy the compiler for using SkiaSharp; imports
+    internal class SkiaSharpDummy
+    {
+    }
+}
